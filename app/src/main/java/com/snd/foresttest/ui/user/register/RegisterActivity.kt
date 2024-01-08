3b647377a7cc481db10def45ac353a2559d4a1e6
dolchvita <yesoe0502@naver.com>
2024-01-08 15:29:17 +0900

    0108 사용자 입력값 DTO 매핑

--- app/src/main/java/com/snd/foresttest/ui/user/register/RegisterActivity.kt
+++ app/src/main/java/com/snd/foresttest/ui/user/register/RegisterActivity.kt
@@ -1,7 +1,6 @@
 package com.snd.foresttest.ui.user.register
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
 import com.snd.foresttest.R
@@ -13,10 +12,12 @@ class RegisterActivity : TMActivity() {
     lateinit var registerBinding : RegisterActBinding
     private val registerVM : RegisterViewModel by viewModels()
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         registerBinding = DataBindingUtil.setContentView(this, R.layout.register_act)
         registerBinding.vm = registerVM
-    }
+        registerBinding.lifecycleOwner = this
 
+
+    }
 }
--- app/src/main/java/com/snd/foresttest/ui/user/register/RegisterViewModel.kt
+++ app/src/main/java/com/snd/foresttest/ui/user/register/RegisterViewModel.kt
@@ -1,33 +1,38 @@
 package com.snd.foresttest.ui.user.register
 
-import androidx.databinding.ObservableField
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.snd.foresttest.base.TMViewModel
 import com.snd.foresttest.data.util.Event
+import com.snd.foresttest.dto.user.UserDTO
 
 class RegisterViewModel : TMViewModel() {
-
-    val id = ObservableField<String>()
-    val pass = ObservableField<String>()
-    val passCheck = ObservableField<String>()
-    val name = ObservableField<String>()
-    val phone = ObservableField<String>()
-    val email = ObservableField<String>()
+    val userId = MutableLiveData<String>()
+    val userPass = MutableLiveData<String>()
+    val userPassCheck = MutableLiveData<String>()
+    val userName = MutableLiveData<String>()
+    val userPhone = MutableLiveData<String>()
+    val userEmail = MutableLiveData<String>()
 
     private val _click = MutableLiveData<Event<Unit>>()
     val click: LiveData<Event<Unit>> get() = _click
 
 
+    fun createUserDTO(): UserDTO {
+        return UserDTO().apply {
+            id = userId.value?: ""
+            password = userPass.value ?: ""
+            name = userName.value ?: ""
+            phone = userPhone.value ?: ""
+            email = userEmail.value ?: ""
+        }
+    }
+
 
     fun onRegiterClick(){
-        log("아이디 확인 1" + id.get())
-        log("아이디 확인 2" + pass.get())
-        log("아이디 확인 3" + passCheck.get())
-        log("아이디 확인 4" + name.get())
-        log("아이디 확인 5" + phone.get())
-
-        //_click.value = Event(Unit)
+        val userDTO = createUserDTO()
+        // 매핑완
+        log("디티오 확인 * " + userDTO )
     }
 
 
